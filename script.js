0fb00320ea83af213593649c8eb4629f1e4910b4
Jetross <jetrossneri07@gmail.com>
2025-03-05 18:50:48 +0800

    added hamburger toggle

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ function updateTime() {
 
 setInterval(updateTime, 1000);
 updateTime();
+
+const hamburger = document.querySelector(".hamburger-menu");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
